f7754c54007e9e73afc72a561f6bba6eb8b5973f
Yury <yakuramori@gmail.com>
2018-11-24 18:20:09 +0000

    Added couple more unit tests

--- tests/DemoWebApi.Service.Tests/MoneyFormattingProcessorTests.cs
+++ tests/DemoWebApi.Service.Tests/MoneyFormattingProcessorTests.cs
@@ -15,8 +15,24 @@ namespace DemoWebApi.Service.Tests
 
         [Theory]
         [InlineData(0, "0.00")]
+        [InlineData(-0, "0.00")]
         [InlineData(10000, "10 000.00")]
+        [InlineData(-10000, "-10 000.00")]
+        [InlineData(123567.567, "123 567.57")]
+        [InlineData(-123567.567, "-123 567.57")]
+        [InlineData(1.0, "1.00")]
+        [InlineData(-1.0, "-1.00")]
+        [InlineData(12345678901, "12 345 678 901.00")]
+        [InlineData(-12345678901.9, "-12 345 678 901.90")]
         public void TestFormatMoney(decimal input, string output)
+        {
+            Assert.Equal(output, _moneyFormatting.FormatMoney(input));
+        }
+
+        [Theory]
+        [InlineData(12345678901234539999.123456789, "12 345 678 901 234 500 000.00")]
+        [InlineData(12345678901234567890.123456789, "12 345 678 901 234 600 000.00")]
+        public void TestFormatMoneyBigNumbers(decimal input, string output)
         {
             Assert.Equal(output, _moneyFormatting.FormatMoney(input));
         }
